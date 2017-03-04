f3df1358a3aa44bf3b1565c5309362553d6c4e02
Colin Heathman <colinheathman@gmail.com>
2017-03-04 16:00:30 -0500

    Leaflet map

---
+++ data.js
@@ -0,0 +1,9 @@
+var data = {
+    "type": "FeatureCollection",
+    "features": [
+        {
+            "type": "Feature",
+            "GeometryCollection": "bluetooth_routes_wgs84.json"
+        }
+    ]
+};
